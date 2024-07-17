7cdd2ceabcd58c6bef17607044e396dbdd56988a
Magdalena Wichrowska <magda-wich@wp.pl>
2024-07-17 12:20:48 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* 
+console.log("Hello World");
+anime({
+    targets: '.imgg',
+    scale: {
+        value: 2,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+    },
+    delay: 250
+});  */
